9f640775ca67f030a2a05e2ed20ee558117603ba
LiuHanzhi <liuhz0123@gmail.com>
2021-11-23 10:52:52 +0800

    app pack

--- plugin/src/main/groovy/com/lhz/MyGradlePlugin.groovy
+++ plugin/src/main/groovy/com/lhz/MyGradlePlugin.groovy
@@ -9,43 +9,48 @@ import org.gradle.api.ProjectConfigurationException
 
 class MyGradlePlugin extends GradlePlugin {
 
+    public static final String DESCRIPTION =  "Make Multi-Channel"
+    public static final String GROUP = "Package"
+
     @Override
     void applyTask(Project project) {
         project.afterEvaluate {
-            project.android.applicationVariants.all { BaseVariant variant ->
+            project.android.applicationVariants.all { BaseVariant vt ->
 
                 println("applyTask start==============")
-                def variantName = variant.name.capitalize();
+                def variantName = vt.name.capitalize();
 
-                if (!isV2SignatureSchemeEnabled(variant)) {
-                    throw new ProjectConfigurationException("Plugin requires 'APK Signature Scheme v2 Enabled' for ${variant.name}.", null);
+                if (!isV2SignatureSchemeEnabled(vt)) {
+                    throw new ProjectConfigurationException("Plugin requires 'APK Signature Scheme v2 Enabled' for ${vt.name}.", null);
                 }
 
                 def taskName = "apk${variantName}"
-                ChannelMaker channelMaker = project.tasks.create(taskName, ChannelMaker);
-                channelMaker.targetProject = project;
-                channelMaker.variant = variant;
-                channelMaker.setup();
+                ChannelMaker channelMaker = project.tasks.create(taskName, ChannelMaker) {
+                    description = DESCRIPTION
+                    group = GROUP
+                    targetProject = project
+                    variant = vt
+                }
 
                 println("addTasks() build type task ${taskName}")
 
-                if (variant.hasProperty('assembleProvider')) {
-                    channelMaker.dependsOn variant.assembleProvider.get()
+                if (vt.hasProperty('assembleProvider')) {
+                    channelMaker.dependsOn vt.assembleProvider.get()
                 } else {
-                    channelMaker.dependsOn variant.assemble
+                    channelMaker.dependsOn vt.assemble
                 }
 
 
-                def buildTypeName = variant.buildType.name
-                println("variant.name:${variant.name}, buildTypeName:${buildTypeName}")
-                if (variant.name != buildTypeName) {
+                def buildTypeName = vt.buildType.name
+                println("variant.name:${vt.name}, buildTypeName:${buildTypeName}")
+                if (vt.name != buildTypeName) {
                     taskName = "apk${buildTypeName.capitalize()}"
 
                     DefaultTask apkTask = project.tasks.findByName(taskName)
                     if (apkTask == null) {
                         apkTask = project.tasks.create(taskName, DefaultTask) {
-                            description "Make Multi-Channel"
-                            group "Package"
+                            description = DESCRIPTION
+                            group = GROUP
                         }
                     }
                     apkTask.dependsOn(channelMaker)
